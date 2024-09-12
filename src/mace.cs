5905bc7033e6571929c284e64780bdea70e3dd44
sures <sures@GARUDADWAJA>
2024-09-12 15:05:05 +0530

    Add project files.

---
+++ src/mace.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Config;
+using Vintagestory.API.Util;
+
+namespace tonwexp.src
+{
+
+    public class mace : ModSystem
+    {
+
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+            api.RegisterItemClass("mace", typeof(MaceItem));
+        }
+
+    }
+    public class MaceItem : Item
+    {
+        public override string GetHeldTpUseAnimation(ItemSlot activeHotbarSlot, Entity byEntity)
+        {
+            return null;
+        }
+        public override bool OnHeldInteractStep(float secondsUsed, ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel)
+        {
+            if (byEntity.World is IClientWorldAccessor)
+            {
+                ModelTransform tf = new ModelTransform();
+                tf.EnsureDefaultValues();
+
+                float offset = GameMath.Clamp(secondsUsed * 4f, 0, 2f);
+
+                //tf.Translation.Set(-offset/4, 0, offset/3);
+                tf.Translation.Set(-offset / 3, 0, offset / 3);
+                tf.Rotation.Set(0, -offset * 15, 0);
+
+                byEntity.Controls.UsingHeldItemTransformBefore = tf;
+            }
+
+            return true;
+        }
+        public override void OnHeldAttackStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, ref EnumHandHandling handling)
+        {
+            byEntity.Attributes.SetInt("didattack", 0);
+
+            byEntity.World.RegisterCallback((dt) =>
+            {
+                IPlayer byPlayer = (byEntity as EntityPlayer).Player;
+                if (byPlayer == null) return;
+
+                if (byEntity.Controls.HandUse == EnumHandInteract.HeldItemAttack)
+                {
+                    byPlayer.Entity.World.PlaySoundAt(new AssetLocation("sounds/player/strike"), byPlayer.Entity, byPlayer, 0.9f + (float)api.World.Rand.NextDouble() * 0.2f, 16, 0.5f);
+                }
+            }, 464);
+
+            handling = EnumHandHandling.PreventDefault;
+        }
+
+        public override bool OnHeldAttackCancel(float secondsPassed, ItemSlot slot, EntityAgent byEntity, BlockSelection blockSelection, EntitySelection entitySel, EnumItemUseCancelReason cancelReason)
+        {
+            return false;
+        }
+
+        public override bool OnHeldAttackStep(float secondsPassed, ItemSlot slot, EntityAgent byEntity, BlockSelection blockSelection, EntitySelection entitySel)
+        {
+            float backwards = -Math.Min(0.35f, 2 * secondsPassed);
+            float stab = Math.Min(1.2f, 20 * Math.Max(0, secondsPassed - 0.35f)); // + Math.Max(0, 5*(secondsPassed - 0.5f));
+
+            if (byEntity.World.Side == EnumAppSide.Client)
+            {
+                IClientWorldAccessor world = byEntity.World as IClientWorldAccessor;
+                ModelTransform tf = new ModelTransform();
+                tf.EnsureDefaultValues();
+
+                float sum = stab + backwards;
+                float ztranslation = Math.Min(0f, 0f * secondsPassed);
+                float easeout = Math.Max(0, 10 * (secondsPassed - 1));
+
+                if (secondsPassed > 0.4f) sum = Math.Max(0, sum - easeout);
+                ztranslation = Math.Max(0, ztranslation - easeout);
+
+                tf.Translation.Set(sum * -0.8f, -0.4f * sum / 1, -ztranslation);
+                tf.Rotation.Set(sum * 25, sum * 2, sum * -50);
+
+                byEntity.Controls.UsingHeldItemTransformBefore = tf;
+
+                if (stab > 1.15f && byEntity.Attributes.GetInt("didattack") == 0)
+                {
+                    world.TryAttackEntity(entitySel);
+                    byEntity.Attributes.SetInt("didattack", 1);
+                    world.AddCameraShake(0.25f);
+                }
+            }
+
+
+
+            return secondsPassed < 1.2f;
+        }
+
+        public override void OnHeldAttackStop(float secondsPassed, ItemSlot slot, EntityAgent byEntity, BlockSelection blockSelection, EntitySelection entitySel)
+        {
+
+        }
+    }
+}
